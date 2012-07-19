aaec8db503ed01201c3808965d2694e5d0b24184
Andrew D. Yates <andrewyates.name@gmail.com>
2012-07-19 18:23:41 -0400

    submodules

---
+++ script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import sys
+
+def main():
+  # load GPLs
+  # load missing row numbers
+  # load graph
+  # map row ids in graph to gene symbols
+  # output result
+  pass
+
+if __name__ == "__main__":
+  main(**dict([s.split('=') for s in sys.argv[1:]]))
