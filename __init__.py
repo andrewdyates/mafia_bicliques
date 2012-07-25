1f18bc04ce4b956fa58d11f614a3f08ca746ab67
Andrew D. Yates <andrewyates.name@gmail.com>
2012-07-24 22:07:59 -0400

    working and tested mafia biclique object

--- __init__.py
+++ __init__.py
@@ -1,12 +1,43 @@
 #!/usr/bin/python
 
+class MafiaBiclique(object):
+  """Bicliques from MAFIA. Rows and columns indexed by zero.
+
+  Note: In graph file, rows are indexed from 1, not zero. Items are indexed from 0.
+  Correct this by subtracting one from every entry in rows.
+  """
+  def __init__(self, fp, n, missing_rows=None):
+    n = int(n)
+    assert fp and n > 0
+    if missing_rows is None:
+      self.missing_rows = set()
+    else:
+      self.missing_rows = set(missing_rows)
+    self.bicliques = [] #rows, cols, density
+    self.row_set, self.col_set = set(), set()
+    self.row_map = make_row_map(n, self.missing_rows)
+    self.bicliques = []
+    
+    for line in fp:
+      rows, cols = [set(map(int, s.strip().split(' '))) for s in line.split(';')]
+      rows = map(lambda x: self.row_map[x-1], rows) # remap rows
+      self.row_set.update(rows)
+      self.col_set.update(cols)
+      self.bicliques.append((rows,cols))
+      
+  def __str__(self):
+    return "{{DensityMergedGraph: " + str(self.bicliques) + "}}"
+
+
 class DensityMergedGraph(object):
   """Bicliques from MAFIA.
 
   Note: In graph file, rows are indexed from 1, not zero. Items are indexed from 0.
   Correct this by subtracting one from every entry in rows.
   """
-  def __init__(self, fp):
+  def __init__(self, fp, missing_rows=None):
+    if missing_rows is None:
+      missing_rows = set([])
     self.bicliques = [] #rows, cols, density
     for line in fp:
       rows, cols, density = map(lambda s: s.strip().split(' '), line.split(';'))
@@ -15,20 +46,30 @@ class DensityMergedGraph(object):
          'cols': map(int, cols),
          'density': float(density[1])}
         )
-    fp.close()
-
-    
   def __str__(self):
     return "{{DensityMergedGraph: " + str(self.bicliques) + "}}"
 
-
+def get_missing_set(fp):
+  return set([int(s.strip()) for s in fp])
+  
 def make_row_map(n, missing_rows):
+  """Map row indices from a truncated row list to rows in the full n-row matrix.
+  Indicies are from 0.
+
+  Args:
+    n: int of total number of rows
+    missing_rows: set([int]) of missing row indices.
+  """
   row_map = {}
   j = 0
+  mapped = set()
   for i in xrange(n):
     if i not in missing_rows:
       row_map[j] = i
       j += 1
+    else:
+      mapped.add(i)
   assert len(row_map) + len(missing_rows) == n
+  assert mapped == set(missing_rows)
   return row_map
 
