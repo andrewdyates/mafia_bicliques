e4c3b9ccb5a1a8e86d07d16a069d8c9c581e35db
Andrew D. Yates <andrewyates.name@gmail.com>
2012-07-25 11:46:16 -0400

    docs

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 """Convert graph row IDs into labels.
 
+WARNING: requires use of GEO_API
+https://github.com/andrewdyates/geo_api
+
 EXAMPLE USE:
 
 python $HOME/mafia_bicliques/script.py gpl_rows_file=$HOME/GPL8178.txt gpl_rows_col=miRNA_ID gpl_cols_file=$HOME/GPL6104.txt gpl_cols_col=Symbol missing_rows_fname=$HOME/gpl8178_gpl6104_spearman_-0.6_missing.txt graph_fname=$HOME/gpl8178_gpl6104_spearman_-0.6_0.05_fci.output
